782c7b3b80b070356cc5021ab5135efafb939bed
maria paula <102986988+mariapaulalaradasilva@users.noreply.github.com>
2024-10-31 11:27:48 -0300

    Add files via upload

--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -1,3 +1,5 @@
+import { getCSS } from "./common.js";
+
 async function quantidadeUsuarios() {
     const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
@@ -5,13 +7,21 @@ async function quantidadeUsuarios() {
     const nomeDasRedes = Object.keys(dados)
     const quantidadeUsuarios = Object.values(dados)
   
-  const data = [
-    {
-      x: nomeDasRedes,
-      y: quantidadeUsuarios,
-      type: 'bar'
-    }
-  ]
+    const data = [
+      {
+        x: 'nomeDasRedes',
+        y: quantidadeDeUsuarios,
+        type: 'bar',
+        marker: {
+          color: getCSS('--primary-color')
+        }
+      }
+    ]
+
+    const layout = {
+      plot_bgcolor: getCSS('--bg-color'),
+      paper_bgcolor: getCSS('--bg-color')
+  }
   
   const grafico = document.createElement('div')
   grafico.className = 'grafico'
